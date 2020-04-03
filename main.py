62cca1c92257eb3e76b087139e2a9492c7beddb6
SlawekMaciejewski <slawek74.m@interia.pl>
2020-04-03 14:49:34 +0200

    Added widget Frame

--- main.py
+++ main.py
@@ -5,12 +5,34 @@ class MyGUI:
     def __init__(self):
         # Creating the main window.
         self.main_window = tkinter.Tk()
-        # Creating a widget Label with the message
-        self.label1 = tkinter.Label(self.main_window, text="Hello world!")
-        self.label2 = tkinter.Label(self.main_window, text="This is GUI!")
+
+        # Creating a widget Frame
+        self.top_frame = tkinter.Frame(self.main_window)
+        self.bottom_frame = tkinter.Frame(self.main_window)
+
+        # Creating a widget Label with the message into Frame
+        self.label1 = tkinter.Label(self.top_frame, text="Hello world!")
+        self.label2 = tkinter.Label(self.top_frame, text="This is GUI!")
+        self.label3 = tkinter.Label(self.top_frame, text="This is first APP!")
+
+        self.label4 = tkinter.Label(self.bottom_frame, text="Winken")
+        self.label5 = tkinter.Label(self.bottom_frame, text="Blinken")
+        self.label6 = tkinter.Label(self.bottom_frame, text="Nod")
+
         # Call to method pack() on the widget Label.
         self.label1.pack(side='right')  # 'bottom', 'top'
         self.label2.pack(side='left')
+        self.label3.pack(side='left')
+
+        self.label4.pack(side='top')
+        self.label5.pack(side='bottom')
+        self.label6.pack(side='top')
+
+
+        # Call to method pack() on the widget Frame.
+        self.top_frame.pack()
+        self.bottom_frame.pack()
+
         # Entrance to the main loop
         tkinter.mainloop()
 
